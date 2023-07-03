38bf4c0e3dc19f203f6584faa2571975bdb3d587
bogdanstojkovski <bogdanstojkovski@gmail.com>
2023-07-03 21:58:44 +0200

    new

--- index.js
+++ index.js
@@ -13,40 +13,44 @@ let lastRecordTime = null;
 const bot = new TelegramBot(telegramToken, {polling: true});
 
 setInterval(async () => {
-  const response = await axios.get(`https://api.airtable.com/v0/${airtableBase}/${airtableTable}`, {
-    headers: {
-      Authorization: `Bearer ${airtableApiKey}`
-    },
-    params: {
-      sort: [{ field: 'createdTime', direction: 'desc' }],
-      maxRecords: 1
-    }
-  });
-
-  const record = response.data.records[0];
-
-  if (!lastRecordTime || new Date(record.createdTime) > lastRecordTime) {
-    lastRecordTime = new Date(record.createdTime);
-
-    const amount = record.fields.Amount;
-    const trafficSource = record.fields.TrafficSource;
-    const buyerName = record.fields.Buyer_Name;
-    const buyerUsername = record.fields.Buyer_Username;
-    const service = record.fields.Service;
-    const serviceInfo = record.fields.ServiceInfo;
-
-    const message = `Paid: ${amount}\nTraffic Source: ${trafficSource}\nBuyer Name: ${buyerName}\nBuyer Username: ${buyerUsername}\nService: ${service}\nServiceInfo: ${serviceInfo}`;
-
-    for (const chatId of chatIds) {
-        await bot.sendMessage(chatId, message, {
-          reply_markup: {
-            inline_keyboard: [[
-              { text: 'Yes', callback_data: `update_yulia_yes:${record.id}` },
-              { text: 'No', callback_data: `update_yulia_no:${record.id}` }
-            ]]
-          }
-        });
+  try {
+    const response = await axios.get(`https://api.airtable.com/v0/${airtableBase}/${airtableTable}`, {
+      headers: {
+        Authorization: `Bearer ${airtableApiKey}`
+      },
+      params: {
+        sort: [{ field: 'createdTime', direction: 'desc' }],
+        maxRecords: 1
       }
+    });
+
+    const record = response.data.records[0];
+
+    if (!lastRecordTime || new Date(record.createdTime) > lastRecordTime) {
+      lastRecordTime = new Date(record.createdTime);
+
+      const amount = record.fields.Amount;
+      const trafficSource = record.fields.TrafficSource;
+      const buyerName = record.fields.Buyer_Name;
+      const buyerUsername = record.fields.Buyer_Username;
+      const service = record.fields.Service;
+      const serviceInfo = record.fields.ServiceInfo;
+
+      const message = `Paid: ${amount}\nTraffic Source: ${trafficSource}\nBuyer Name: ${buyerName}\nBuyer Username: ${buyerUsername}\nService: ${service}\nServiceInfo: ${serviceInfo}`;
+
+      for (const chatId of chatIds) {
+          await bot.sendMessage(chatId, message, {
+            reply_markup: {
+              inline_keyboard: [[
+                { text: 'Yes', callback_data: `update_yulia_yes:${record.id}` },
+                { text: 'No', callback_data: `update_yulia_no:${record.id}` }
+              ]]
+            }
+          });
+        }
+      }
+    } catch (err) {
+      console.log('Error', err.message);
     }
   }, 10000);
 
@@ -55,27 +59,34 @@ bot.on('callback_query', async (callbackQuery) => {
   const recordId = callbackQuery.data.split(':')[1];
 
   if (action === 'update_yulia_yes') {
-    await axios.patch(`https://api.airtable.com/v0/${airtableBase}/${airtableTable}/${recordId}`, {
-      fields: {
-        Yulia: true
-      }
-    }, {
-      headers: {
-        Authorization: `Bearer ${airtableApiKey}`,
-        'Content-Type': 'application/json'
-      }
-    });
+    try {
+      await axios.patch(`https://api.airtable.com/v0/${airtableBase}/${airtableTable}/${recordId}`, {
+        fields: {
+          Yulia: true
+        }
+      }, {
+        headers: {
+          Authorization: `Bearer ${airtableApiKey}`,
+          'Content-Type': 'application/json'
+        }
+      });
+    } catch (err) {
+      console.log('Error', err.message);
+    }
   }
 
   bot.answerCallbackQuery(callbackQuery.id);
 });
 
 const app = express();
-app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
     var result = 'App is running'
     response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function() {
+    console.log('App is running, server is listening on port ', PORT);
 });
